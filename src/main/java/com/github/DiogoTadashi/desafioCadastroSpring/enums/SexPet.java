f036db0ba9022b68418780518b043476e268e04b
DiogoTadashi <diogotadashi22@gmail.com>
2026-05-28 18:22:48 -0300

    feat: enums

---
+++ src/main/java/com/github/DiogoTadashi/desafioCadastroSpring/enums/SexPet.java
@@ -0,0 +1,7 @@
+package com.github.DiogoTadashi.desafioCadastroSpring.enums;
+
+public enum SexPet {
+    MALE,
+    FEMALE,
+    INDETERMINADO
+}
---
+++ src/main/java/com/github/DiogoTadashi/desafioCadastroSpring/enums/TypePet.java
@@ -0,0 +1,6 @@
+package com.github.DiogoTadashi.desafioCadastroSpring.enums;
+
+public enum TypePet {
+    CACHORRO,
+    GATO
+}
