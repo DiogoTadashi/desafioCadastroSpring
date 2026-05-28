dfbe4e5fbb9816e79d9a0c6bbcdd1be5225faaf8
DiogoTadashi <diogotadashi22@gmail.com>
2026-05-28 16:58:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desafioCadastroSpring
+API REST desenvolvida com Spring Boot para gerenciamento de pets, utilizando PostgreSQL, Docker, JPA/Hibernate e testes unitários.
---
+++ src/test/java/com/github/DiogoTadashi/desafioCadastroSpring/DesafioCadastroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.DiogoTadashi.desafioCadastroSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCadastroSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
